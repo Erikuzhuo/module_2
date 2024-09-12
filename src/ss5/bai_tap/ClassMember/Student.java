f9979babc318cbdbd57e596a0c4082cc75bd2b21
erikuzhuo <erikuzhuo01@gmail.com>
2024-09-12 21:39:14 +0800

    nop bai ss5

---
+++ src/ss5/bai_tap/ClassMember/Student.java
@@ -0,0 +1,40 @@
+package ss5.bai_tap.ClassMember;
+
+public class Student {
+    private String name;
+    private String classes;
+
+    public Student(){
+        String name = "John";
+        String classes = "C02";
+    }
+
+    public Student(String name, String classes) {
+        this.name = name;
+        this.classes = classes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ src/ss5/bai_tap/ClassMember/test.java
@@ -0,0 +1,10 @@
+package ss5.bai_tap.ClassMember;
+
+public class test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Michael");
+        student.setClasses("C06");
+        System.out.println(student.toString());
+    }
+}
