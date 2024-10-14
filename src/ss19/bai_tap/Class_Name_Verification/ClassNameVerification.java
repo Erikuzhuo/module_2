b6f643ae17b7e7909737ebf0d6fcd49cd0f83c14
erikuzhuo <erikuzhuo01@gmail.com>
2024-10-14 20:00:51 +0800

    bai 19

---
+++ src/ss19/bai_tap/Class_Name_Verification/ClassNameVerification.java
@@ -0,0 +1,23 @@
+package ss19.bai_tap.Class_Name_Verification;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ClassNameVerification {
+    public static void main(String[] args) {
+        boolean check;
+        Matcher matcher;
+        String className;
+        do {
+                Scanner scanner = new Scanner(System.in);
+            String regex = "[ACP][0-9]{4}[G-K]";
+            System.out.println("Input Class Name: ");
+            className = new String(scanner.nextLine());
+            Pattern pattern = Pattern.compile(regex);
+            matcher = pattern.matcher(className);
+        } while (check = !matcher.matches());
+        System.out.println("Class "+className +" already created.");
+
+    }
+}
